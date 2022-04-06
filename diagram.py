9f190915c85a9a8b42c9aa19f84d130cdf2563cf
amerinor01 <amerinor01@gmail.com>
2022-04-06 14:24:02 +0200

    update diagram2

--- diagram.py
+++ diagram.py
@@ -3,9 +3,9 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
+with Diagram("Grouped Workers.pdf", show=False, direction="TB"):
     ELB("lb") >> [EC2("worker1"),
                   EC2("worker2"),
                   EC2("worker3"),
                   EC2("worker4"),
-                  EC2("worker5")] >> RDS("events") >> EC("temporal worker")
+                  EC2("worker5")] >> RDS("events")
