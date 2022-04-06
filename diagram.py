213cc5574128ba0bd7fdda224fe6e65cb530d1b7
amerinor01 <amerinor01@gmail.con>
2022-04-06 20:27:30 +0200

    create the diagram.py [for the next time we need to fix the CI to dont need the png updated]

--- diagram.py
+++ diagram.py
@@ -1,11 +1,63 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+import os
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import EC2, Lambda
+from diagrams.aws.database import DDB
+from diagrams.aws.storage import S3
+from diagrams.aws.network import ELB, APIGateway
+from diagrams.aws.general import Users
+from diagrams.aws.security import Cognito
+from diagrams.aws.integration import SQS
+with Diagram("Infraestructure Diagram", show=False):
 
-with Diagram("Grouped Workers.pdf", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+    # Users model
+    users = Users("Usuarios")
+
+    # All the stuff that is on the aws Infraestructure
+    with Cluster('aws'):
+
+        # User validator
+        iam = Cognito('User validator')
+
+        # Api Gateway
+        api_gw = APIGateway("Api-Gateway")
+
+        # All the endpoints are lambda functions
+        with Cluster("funciones"):
+            workers = [Lambda("Upload video"),
+                       Lambda("View my videos"),
+                       Lambda("View all videos")]
+
+        # Load Balancers                       
+        elb = ELB('Load balancer')
+
+        # All the vs with the trained model
+        with Cluster("Workers"):
+            ecs = [EC2('Trained model'),
+                   EC2('Trained model'),
+                   EC2('Trained model'),
+                   EC2('Trained model')]
+        # The S3 sotorage
+        main_storage = S3('Video storage')
+
+        storaged_trigger = Lambda("Send video to get faces")
+
+        # Dynamo DB
+        dynamo = DDB('processed data')
+
+        getvideoMetadata = Lambda('Inform users')
+
+        # SQS
+        queue = SQS('email message')
+
+
+
+    # Relations
+    users << queue 
+    users >> api_gw >> workers
+    api_gw >> iam >> api_gw
+
+    workers[0] >> main_storage >> storaged_trigger >> elb 
+    elb >> ecs >> dynamo >>  getvideoMetadata >> queue
+    
+    workers[1:] >> main_storage
+    workers[1:] >> dynamo
