99bdab6431b28b59585a8ea28f7a3095e9882c46
amerinor01 <amerinor01@gmail.con>
2022-06-04 19:47:35 +0200

    removed unused inclues and add new api endpoint

--- serverless/handlers/send_email.py
+++ serverless/handlers/send_email.py
@@ -1,20 +1,7 @@
-import json
-import logging
-import boto3
-import os
-from datetime import datetime
-
-def handler(event, context):
-   client = boto3.client('sns')
-
-   person = event['Records'][0]['dynamodb']['Keys']['PersonId']['S']
-   person = '.'.join(person.split('.')[:-1])
-
-   message = "Usted el usuario {} ha aparecido en la cámara a la hora {}.\n".format(person, 
-                                                                                    str(datetime.fromtimestamp(float(event['Records'][0]['dynamodb']['Keys']['Time']['N']))))
+                                                                             str(datetime.fromtimestamp(float(event['Records'][0]['dynamodb']['Keys']['Time']['N']))))
     
    topics = client.list_topics()
-   
+
    for topic in topics['Topics']:
       person = event['Records'][0]['dynamodb']['Keys']['PersonId']['S']
       person = ''.join(person.split('.')[:-1])
@@ -24,4 +11,14 @@ def handler(event, context):
             TopicArn = topic['TopicArn'],
             Message = message,
             Subject='Fichaje - FaaS'
-         )
+         )import boto3
+from datetime import datetime
+
+def handler(event, context):
+   client = boto3.client('sns')
+
+   person = event['Records'][0]['dynamodb']['Keys']['PersonId']['S']
+   person = '.'.join(person.split('.')[:-1])
+
+   message = "Usted el usuario {} ha aparecido en la cámara a la hora {}.\n".format(person, 
+       
